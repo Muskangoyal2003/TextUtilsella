1bddbc4a2e955051490a60bbb3de535afb043cb5
Muskan <itsmuskangoyal2003@gmail.com>
2023-03-09 21:34:33 +0530

    Updates

---
+++ precache-manifest.6551e24af580d912c95ea31b28bc770a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "590820aafb0f06a5f168",
+    "url": "/TextUtilsella/static/css/main.6ac0cad2.chunk.css"
+  },
+  {
+    "revision": "590820aafb0f06a5f168",
+    "url": "/TextUtilsella/static/js/main.590820aa.chunk.js"
+  },
+  {
+    "revision": "ad17ecab6b05774eaf3d",
+    "url": "/TextUtilsella/static/js/1.ad17ecab.chunk.js"
+  },
+  {
+    "revision": "0a05abbf2ee0b322f5e0",
+    "url": "/TextUtilsella/static/js/runtime~main.0a05abbf.js"
+  },
+  {
+    "revision": "f2f2302eaec569ba2d095711449cd6a8",
+    "url": "/TextUtilsella/index.html"
+  }
+];
